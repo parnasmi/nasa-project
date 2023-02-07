edecac5b386ac8d8a17879ccfa02d1593b1d549e
Maksadkulov Ilhom <parnasmi@gmail.com>
2023-02-07 07:53:13 +0500

    add morgan logging

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
+const path = require("path");
+const morgan = require("morgan");
 
-app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
+
+app.use(morgan("combined"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
+
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
